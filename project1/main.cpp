fbb03f2df6fabcb5d1190718f45e9bdb0a84fe3a
Hunter Bowman <40217936+hunterbowman@users.noreply.github.com>
2018-10-12 16:52:10 -0700

    Start of Poject1

---
+++ project1/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include"heap.h"
+#include "util.h"
+
+using namespace std;
+
+
+
+int main {
+
+//ELEMENT * A = new ELEMENT[length + 1]
+// in my arrays, the first index[0] can be used to store the size of the array.
+//Clion, jetbrains
+
+
+
+}
